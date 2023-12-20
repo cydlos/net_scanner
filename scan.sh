18070ba13599f8d9a859f40867b24e9d636dd40e
cyd losekann <cydnel@gmail.com>
2023-12-20 03:17:32 -0300

    new loop

--- scan.sh
+++ scan.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+read -p "Enter the base IP address (e.g., 192.168.1): " base_ip
+
+if [ -z "$base_ip" ]; then
+    echo "Error: Base IP address is required."
+    exit 1
+fi
+
 for i in {1..254}
 do
-    ping -c 1 "$1.$i" | grep ttl | cut -d " " -f 4 | cut -d ":" -f 1 &
+    ping -c 1 "$base_ip.$i" | grep ttl | cut -d " " -f 4 | cut -d ":" -f 1 &
 done
